37bbc254e3212f5f5315aac49c2ef5800e4721c1
Kuziutina <cuzyutinasofia2@yandex.ru>
2019-01-07 21:01:21 +0300

    test git

--- src/main/java/ru/kpfu/itis/androidlab/Join/service/UserService.java
+++ src/main/java/ru/kpfu/itis/androidlab/Join/service/UserService.java
@@ -234,6 +234,7 @@ public class UserService implements UserServiceInt {
                 }
             }
         }
+        
         if (level != null) {
             users = resultUser;
             resultUser = new ArrayList<>();
