2578c0de9846e6d334aa4a53d6bf0d9f4112e79a
Kuziutina <cuzyutinasofia2@yandex.ru>
2019-03-26 21:14:51 +0300

    fix Json view in push notification

--- src/main/java/ru/kpfu/itis/androidlab/Join/helper/NotificationHelper.java
+++ src/main/java/ru/kpfu/itis/androidlab/Join/helper/NotificationHelper.java
@@ -1,10 +1,14 @@
 package ru.kpfu.itis.androidlab.Join.helper;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.json.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import ru.kpfu.itis.androidlab.Join.dto.ProjectDto;
+import ru.kpfu.itis.androidlab.Join.dto.UserDto;
 import ru.kpfu.itis.androidlab.Join.model.Notification;
 import ru.kpfu.itis.androidlab.Join.model.User;
 
@@ -21,6 +25,7 @@ public class NotificationHelper {
     }
 
     public ResponseEntity<String> send(User user, Notification notification) {
+        ObjectMapper objectMapper = new ObjectMapper();
         JSONObject body = new JSONObject();
         body.put("to", user.getTokenDevice());
         body.put("priority", "high");
@@ -34,8 +39,13 @@ public class NotificationHelper {
 
         data.put("id", notification.getId());
         data.put("type", notification.getType());
-        data.put("user", notification.getUser());
-        data.put("project", notification.getProject());
+        try {
+            data.put("user", objectMapper.writeValueAsString(UserDto.from(notification.getUser())));
+            data.put("project", objectMapper.writeValueAsString(ProjectDto.from(notification.getProject())));
+
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
 
         body.put("data", data);
         HttpEntity<String> request = new HttpEntity<>(body.toString());
