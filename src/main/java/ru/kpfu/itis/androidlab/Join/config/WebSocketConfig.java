de5e00e5fe54065671c9e6e16d9dc353685a5593
Kuziutina <cuzyutinasofia2@yandex.ru>
2019-05-04 23:28:10 +0300

    heroku failed, temp fix

--- src/main/java/ru/kpfu/itis/androidlab/Join/config/WebSocketConfig.java
+++ src/main/java/ru/kpfu/itis/androidlab/Join/config/WebSocketConfig.java
@@ -1,5 +1,5 @@
 package ru.kpfu.itis.androidlab.Join.config;
-/*
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
@@ -21,5 +21,3 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/socket-end-point").setAllowedOrigins("*").withSockJS();
     }
 }
-*/
-public class WebSocketConfig{}
--- src/main/java/ru/kpfu/itis/androidlab/Join/controller/ChatWebSocketController.java
+++ src/main/java/ru/kpfu/itis/androidlab/Join/controller/ChatWebSocketController.java
@@ -1,7 +1,12 @@
 package ru.kpfu.itis.androidlab.Join.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import ru.kpfu.itis.androidlab.Join.dto.MessageDto;
+import ru.kpfu.itis.androidlab.Join.model.User;
 import ru.kpfu.itis.androidlab.Join.service.MessageService;
 import ru.kpfu.itis.androidlab.Join.service.UserService;
 import ru.kpfu.itis.androidlab.Join.service.interfaces.UserServiceInt;
@@ -19,7 +24,7 @@ public class ChatWebSocketController {
         this.messageService = messageService;
     }
 
-/*
+
     @MessageMapping("/openApp/{userId}")
     public void notificationMessageOff(String m, @PathVariable Long userId) {
         User user = userService.getUser(userId);
@@ -41,5 +46,4 @@ public class ChatWebSocketController {
         User user = userService.getUser(userId);
         user.setOnline(false);
     }
-    */
 }
