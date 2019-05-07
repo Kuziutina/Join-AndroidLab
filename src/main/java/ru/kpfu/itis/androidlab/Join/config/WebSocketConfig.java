9ff7d5317cf729a2514dc4a05573e72dfd2ad5f4
Kuziutina <cuzyutinasofia2@yandex.ru>
2019-05-07 23:24:29 +0300

    test webSocketSecurity

--- src/main/java/ru/kpfu/itis/androidlab/Join/config/WebSocketConfig.java
+++ src/main/java/ru/kpfu/itis/androidlab/Join/config/WebSocketConfig.java
@@ -2,13 +2,15 @@ package ru.kpfu.itis.androidlab.Join.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+public class WebSocketConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -20,4 +22,22 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/socket-end-point").setAllowedOrigins("*").withSockJS();
     }
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages
+                .nullDestMatcher().authenticated()
+                .simpSubscribeDestMatchers("/*").permitAll()
+                .simpDestMatchers("/**").permitAll()
+//            .simpSubscribeDestMatchers("/user/**", "/topic/friends/*").hasRole("USER")
+//            .simpTypeMatchers(MESSAGE, SUBSCRIBE).denyAll()
+                .anyMessage().permitAll();
+
+    }
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+
 }
