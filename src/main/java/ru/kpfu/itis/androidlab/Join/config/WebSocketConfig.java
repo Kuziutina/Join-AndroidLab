bb2ee15622438690f099a224d1c83d439e11bd3d
Kuziutina <cuzyutinasofia2@yandex.ru>
2019-05-07 23:38:05 +0300

    test webSocketSecurity

--- src/main/java/ru/kpfu/itis/androidlab/Join/config/WebSocketConfig.java
+++ src/main/java/ru/kpfu/itis/androidlab/Join/config/WebSocketConfig.java
@@ -3,14 +3,13 @@ package ru.kpfu.itis.androidlab.Join.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
-import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
@@ -21,23 +20,6 @@ public class WebSocketConfig extends AbstractSecurityWebSocketMessageBrokerConfi
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/socket-end-point").setAllowedOrigins("*").withSockJS();
-    }
-
-    @Override
-    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
-        messages
-                .nullDestMatcher().authenticated()
-                .simpSubscribeDestMatchers("/*").permitAll()
-                .simpDestMatchers("/**").permitAll()
-//            .simpSubscribeDestMatchers("/user/**", "/topic/friends/*").hasRole("USER")
-//            .simpTypeMatchers(MESSAGE, SUBSCRIBE).denyAll()
-                .anyMessage().permitAll();
-
-    }
-
-    @Override
-    protected boolean sameOriginDisabled() {
-        return true;
     }
 
 }
